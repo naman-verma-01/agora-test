2901be8c74166acf575e5d7de970ba77eabddc3f
naman-verma-01 <namanvermaca@gmail.com>
2023-04-10 21:18:25 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import AgoraUIKit from "agora-react-uikit";
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  const [videoCall, setVideoCall] = useState(true);
+
+  const callbacks = {
+    EndCall: () => setVideoCall(false),
+  };
+  const rtcProps = {
+    appId: "6d076e70e67342b7ba83ed8b52687cc6",
+    channel: "test-agora-channel",
+    token: "007eJxTYDjdtyp5+bXfe5nu3XkZP9cw9MvUw84bkw2TRe5MCJQK/mukwGCWYmBulmpukGpmbmxilGSelGhhnJpikWRqZGZhnpxsZqJnktIQyMgwc8cHRkYGCATxhRhKUotLdBPT84sSdZMzEvPyUnMYGADlQiY6",
+  };
+
+
+  return videoCall ? (
+    <div style={{ display: "flex", width: "100vw", height: "100vh" }}>
+      <AgoraUIKit rtcProps={rtcProps} callbacks={callbacks} />
+
     </div>
+  ) : (
+    <h3 onClick={() => setVideoCall(true)}>Join</h3>
   );
 }
 
